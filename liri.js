029ea30a312f4a02b985f44e9e678037c440235d
26735jmc <13514717+jmcoleman@users.noreply.github.com>
2018-07-21 09:06:11 -0400

    added logging

--- liri.js
+++ liri.js
@@ -1,64 +1,92 @@
+//////////////////////////
+// npm require
+//////////////////////////
 require("dotenv").config();
-
 var request = require("request");
 var fs = require("fs");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
+
+///////////////////////////////
+// constants and variables
+///////////////////////////////
 const COMMAND_LINE_ORIGIN = "command_line";
 const FILE_ORIGIN = "file";
 const DEFAULT_SONG = "The Sign";
 const DEFAULT_ARTIST = "Ace of Base";
 
-// grab the arguments on the command line
-var args = process.argv;
-// console.log(keys);
-
-// check that a command is given
-if (process.argv[2] === undefined) {
-    console.log("Please provide a command.  \nOptions are: 'my-tweets','spotify-this-song', 'movie-this', 'do-what-it-says'"); 
-    return;
-} else {
-    var command = process.argv[2];
+var cmdObj = {
+    command: '',
+    options: []
 }
 
-processCommand(command, process.argv, COMMAND_LINE_ORIGIN);
+///////////////////////////////////
+// check that a command is given
+///////////////////////////////////
+
+// load command and options if present
+if (process.argv[2] === undefined) {
+    console.log("Please provide a command.\nOptions are: 'my-tweets','spotify-this-song', 'movie-this', 'do-what-it-says'"); 
+    return;
+} else {
+    cmdObj.command = process.argv[2];
+    if (process.argv.length > 3) {
+        cmdObj.options = process.argv.slice(3,process.argv.length + 1);
+    }
+    console.log("------------------------------");
+    console.log("[COMMAND LINE arguments]");
+    console.log("command: " + cmdObj.command);
+    console.log("options: " + cmdObj.options);
+}
+
+// processCommand(cmdObj.command, cmdObj.options, COMMAND_LINE_ORIGIN);
+processCommand(cmdObj);
 
 /////////////////////////////////
 // Functions
 /////////////////////////////////
-function processCommand(command, arguments, origin) {
+function processCommand(commandConfig) {
 
     // process the given command
-    switch (command) {
+    switch (commandConfig.command) {
 
         case 'my-tweets':
-            console.log("in my-tweets");
-            var tweet = '';
+            // console.log("in my-tweets");
 
-            var client = new Twitter(keys.twitter);
-            console.log(client);
+            // var client = new Twitter(keys.twitter);
+            // console.log(client);
 
-            // when in file will be in index 1 but when in command line will be in position 3
-            if (origin === FILE_ORIGIN) {
-                // console.log("Argument1: " + arguments[1]);
-                tweet = arguments[1];
-            } else if (origin === COMMAND_LINE_ORIGIN) {
-                // console.log("Argument3: " + arguments[3]);
-                tweet = arguments[3];
-            }
+            var tweet = (commandConfig.options[0] === undefined) ? "" : commandConfig.options[0];
 
             // show last 20 tweets and when they were created
 
 
+            ///////////////////////////
+            // console log output
+            ///////////////////////////
+            console.log("------------------------------");
+            console.log("***** TWEETS *****");
             
+            ///////////////////////
+            // log tweet output
+            ///////////////////////
+            var logData = "***** TWEETS *****" + "\n" ;
+                // + "Artist(s): " + getArtists(item.artists) + "\n"
+                // + "Song Name: " + item.name + "\n"
+                // + "Preview Link: " + item.preview_url + "\n"
+                // + "Album: " + item.album.name;
+            logOutput(logData);
+
+            // logOutput("***** TWEETS *****");
+            // logOutput("Artist(s): " + getArtists(item.artists));
+            // logOutput("Song Name: " + item.name);
+            // logOutput("Preview Link: " + item.preview_url);
+            // logOutput("Album: " + item.album.name);
+
             break;
 
         case 'spotify-this-song':
-            console.log("spotify-this-song");
-            var song_title = '';
-
-            var spotify = new Spotify(keys.spotify);
-            // console.log(spotify);
+            // console.log("in spotify-this-song");
 
             ///////////////////////////////////
             // functions
@@ -75,28 +103,47 @@ function processCommand(command, arguments, origin) {
             }
 
             function getTrack(item,index) {
+                ///////////////////////////
+                // console log output
+                ///////////////////////////
+
                 // console.log("Track " + index + ": " + JSON.stringify(item));
                 // console.log("-------------------------------------------------------------------------------\nTrack " + index + ": ");
                 // console.dir(item);
 
                 //show output for spotify
+                console.log("------------------------------");
+                console.log("***** SONG *****");
                 console.log("Artist(s): " + getArtists(item.artists));
                 console.log("Song Name: " + item.name);
                 console.log("Preview Link: " + item.preview_url);
                 console.log("Album: " + item.album.name);
+
+                ///////////////////////
+                // log song output
+                ///////////////////////
+                var logData = "***** SONG *****" + "\n" 
+                    + "Artist(s): " + getArtists(item.artists) + "\n"
+                    + "Song Name: " + item.name + "\n"
+                    + "Preview Link: " + item.preview_url + "\n"
+                    + "Album: " + item.album.name;
+                logOutput(logData);
+
+                // logOutput("***** SONG *****");
+                // logOutput("Artist(s): " + getArtists(item.artists));
+                // logOutput("Song Name: " + item.name);
+                // logOutput("Preview Link: " + item.preview_url);
+                // logOutput("Album: " + item.album.name);
             }
 
-            // when in file will be in index 1 but when in command line will be in position 3
-            if (origin === FILE_ORIGIN) {
-                // console.log("Argument1: " + arguments[1]);
-                song_title = arguments[1];
-            } else if (origin === COMMAND_LINE_ORIGIN) {
-                // console.log("Argument3: " + arguments[3]);
-                song_title = arguments[3];
-            }
+            ////////////////////////////////
+            // search Spotify
+            ////////////////////////////////
+            var spotify = new Spotify(keys.spotify);
+            // console.log(spotify);
 
-            var title = (song_title === undefined) ? "track:" + DEFAULT_SONG + ' artist:' + DEFAULT_ARTIST : "track:" + song_title;
-            console.log("TITLE: " + title);
+            var title = (commandConfig.options[0] === undefined) ? "track:" + DEFAULT_SONG + ' artist:' + DEFAULT_ARTIST : "track:" + commandConfig.options[0];
+            // console.log("TITLE: " + title);
 
             spotify
                 .search({type: 'track', query: title, limit: 1})
@@ -111,32 +158,29 @@ function processCommand(command, arguments, origin) {
             break;
 
         case 'movie-this':
-            console.log("movie-this");
-            var movie_title = '';
+            // console.log("in movie-this");
 
-            // when in file will be in index 1 but when in command line will be in position 3
-            if (origin === FILE_ORIGIN) {
-                // console.log(arguments[1]);
-                movie_title = arguments[1];
-            } else if (origin === COMMAND_LINE_ORIGIN) {
-                // console.log(arguments[3]);
-                movie_title = arguments[3];
-            }
-            
-            var title = (movie_title === undefined) ? "Mr. Nobody" : movie_title;
+            /////////////////////////////////////
+            // search for movies in OMDB api
+            /////////////////////////////////////
+            var title = (commandConfig.options[0] === undefined) ? "Mr. Nobody" : commandConfig.options[0];
             var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&type=movie&r=json&apikey=a5f801e8";
-            console.log(queryUrl);
+            // console.log(queryUrl);
 
             request(queryUrl, function(err, response, body) {
-                // If the request is unsuccessful
-                if (err) {
-                    return console.log(err);
-                }
+                // if the request is unsuccessful
+                if (err) { return console.log(err); }
                 // debugger;
+
                 // If the request is successful
                 if (!err && response.statusCode === 200) {
                     var movie = JSON.parse(body);
 
+                    ///////////////////////////
+                    // console log output
+                    ///////////////////////////
+                    console.log("------------------------------");
+                    console.log("***** MOVIE *****");
                     console.log("Title: " + movie.Title);
                     console.log("Release Date: " + movie.Year);
                     console.log("IMDB Rating: " + movie.imdbRating);
@@ -147,28 +191,81 @@ function processCommand(command, arguments, origin) {
                     console.log("Language: " + movie.Language);
                     console.log("Plot: " + movie.Plot);
                     console.log("Actors: " + movie.Actors);
+
+                    ///////////////////////
+                    // log movie output
+                    ///////////////////////
+                    var logData = "***** MOVIE *****" + "\n" 
+                        + "Title: " + movie.Title + "\n"
+                        + "Release Date: " + movie.Year + "\n"
+                        + "IMDB Rating: " + movie.imdbRating + "\n"
+                        + "Rotten Tomatoes Rating: " + movie.Ratings[1].Value + "\n"
+                        + "Production Country: " + movie.Country + "\n"
+                        + "Language: " + movie.Language + "\n"
+                        + "Plot: " + movie.Plot + "\n"
+                        + "Actors: " + movie.Actors;
+                    logOutput(logData);
+
+                    // logOutput("***** MOVIE *****");
+                    // logOutput("Title: " + movie.Title);
+                    // logOutput("Release Date: " + movie.Year);
+                    // logOutput("IMDB Rating: " + movie.imdbRating);
+                    // logOutput("Rotten Tomatoes Rating: " + movie.Ratings[1].Value);     
+                    // logOutput("Production Country: " + movie.Country);
+                    // logOutput("Language: " + movie.Language);
+                    // logOutput("Plot: " + movie.Plot);
+                    // logOutput("Actors: " + movie.Actors);
                 }
             });
             break;
 
         case 'do-what-it-says':
-            console.log("do-what-it-says");
+            // console.log("in do-what-it-says");
 
             // process the file   
             fs.readFile("random.txt","utf8", function(err, data) {
                 if (err) { return console.log(err); }
 
+                ///////////////////////////////////////////////
+                // read command and option(s) from file
+                ///////////////////////////////////////////////
                 var output = data.split(",");
-
                 var cmd = output[0];
 
-                console.log("--------------------\nRead FILE:");
-                for (var i=0; i < output.length; i++) {
-                    console.log(output[i]);
-                }
-                console.log("--------------------");
+                if (output[0] === undefined) {
+                    console.log("File must contain a valid command.\nOptions are: 'my-tweets','spotify-this-song', 'movie-this', 'do-what-it-says'"); 
+                    return;
+                } else {
+                    cmdObj.command = output[0];
+                    if (output.length > 1) {
+                        cmdObj.options = output.slice(1,output.length + 1);
+                    }
 
-                processCommand(cmd,output,FILE_ORIGIN);
+                    ///////////////////////////
+                    // console log output
+                    ///////////////////////////
+                    console.log("------------------------------");
+                    console.log("***** DO WHAT IT SAYS *****");
+                    console.log("Command: " + cmdObj.command);
+                    console.log("Options: " + cmdObj.options);
+
+                    ////////////////////////////////
+                    // log read from file output
+                    ////////////////////////////////
+                    var logData = "***** DO WHAT IT SAYS *****" + "\n" 
+                        + "Command: " + cmdObj.command + "\n"
+                        + "Options: " + cmdObj.options;
+                    logOutput(logData);
+
+                    // logOutput("***** DO WHAT IT SAYS *****");
+                    // logOutput("Command: " + cmdObj.command;
+                    // logOutput("Options: " + cmdObj.options);
+                }
+
+                /////////////////////////////////////
+                // do action read from file
+                /////////////////////////////////////
+                processCommand(cmdObj);
             });
             break;
 
@@ -178,9 +275,14 @@ function processCommand(command, arguments, origin) {
 }
 
 function logOutput (msg) {
+    // append each logged output on own line
+    msg = "------------------------------\n" + msg + "\n";
+
     // Add to file and if the file didn't exist then it gets created on the fly.
-    fs.appendFile("log.txt", msg, function(err) {
+    fs.appendFile("log.txt", msg, 'utf8', function(err) {
         if (err) { console.log(err); }
-        else { console.log("Content Added!"); }
+        else { 
+            console.log("Logged data!"); 
+        }
     });
 };
