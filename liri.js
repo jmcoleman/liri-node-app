7675802fe8991047f8e4291b330fce215d54ad0d
26735jmc <13514717+jmcoleman@users.noreply.github.com>
2018-07-20 21:43:26 -0400

    spotify api connected

--- liri.js
+++ liri.js
@@ -3,35 +3,21 @@ require("dotenv").config();
 var request = require("request");
 var fs = require("fs");
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 
 // grab the arguments on the command line
 var args = process.argv;
-var cmd;
+// console.log(keys);
 
 // check that a command is given
 if (process.argv[2] === undefined) {
-    console.log("Please provide a command.  \nOptions are: 'my-tweets','spotify-this-song', 'movie-this', 'do-what-it-says', 'end-game'"); 
+    console.log("Please provide a command.  \nOptions are: 'my-tweets','spotify-this-song', 'movie-this', 'do-what-it-says'"); 
     return;
 } else {
-    cmd = process.argv[2];
+    var command = process.argv[2];
 }
 
-processCommand(cmd, process.argv);
-
-// process the file   
-fs.readFile("random.txt","utf8", function(err, data) {
-    if (err) { return console.log(err); }
-
-    var output = data.split(",");
-
-    cmd = output[0];
-
-    for (var i=0; i < output.length; i++) {
-        console.log(output[i]);
-    }
-
-    processCommand(cmd,output);
-});
+processCommand(command, process.argv);
 
 /////////////////////////////////
 // Functions
@@ -46,12 +32,30 @@ function processCommand(command, arguments) {
             break;
         case 'spotify-this-song':
             console.log("spotify-this-song");
-            // var spotify = new Spotify(keys.spotify);
+            var spotify = new Spotify(keys.spotify);
             // console.log(spotify);
+            
+            function getTrack(item, index) {
+                // console.log("Track " + index + ": " + JSON.stringify(item));
+                console.log("-------------------------------------------------------------------------------\nTrack " + index + ": ");
+                console.dir(item);
+            }
+
+            console.log(arguments[1]);
+            spotify
+                .search({type: 'track', query: arguments[1], limit: 10})
+                .then(function(data) {
+                    // console.log(data); 
+                    // console.log(data.tracks.items);
+                    data.tracks.items.forEach(getTrack);
+                })
+                .catch(function(err) {
+                    console.error('Error occurred: ' + err); 
+                });
             break;
         case 'movie-this':
             console.log("movie-this");
-            
+
             var title = (arguments[3] === undefined) ? "Mr. Nobody" : args[3];
             var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&type=movie&r=json&apikey=a5f801e8";
             console.log(queryUrl);
@@ -82,11 +86,23 @@ function processCommand(command, arguments) {
         case 'do-what-it-says':
             console.log("do-what-it-says");
 
-            break;
-        case 'end-game':
-            console.log("end-game");
+            // process the file   
+            fs.readFile("random.txt","utf8", function(err, data) {
+                if (err) { return console.log(err); }
 
-            return;
+                var output = data.split(",");
+
+                var cmd = output[0];
+
+                console.log("--------------------\nRead FILE:");
+                for (var i=0; i < output.length; i++) {
+                    console.log(output[i]);
+                }
+                console.log("--------------------");
+
+                processCommand(cmd,output);
+            });
+            break;
         default:
             console.log("invalid answer");    
     }
